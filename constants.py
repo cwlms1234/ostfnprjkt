78e10937af867b94d4c0ff11be2cf24c87cd179d
cwlms1234 <chwilms@web.de>
2024-11-16 20:38:55 +0100

    barebones

---
+++ constants.py
@@ -0,0 +1,9 @@
+class TemperatureThresholds:
+    lower_limit: 20
+    upper_limit: 30
+    alert_limit: 40
+    corridor_size: 5
+
+class TestValues: # TODO remove after testing
+    upper: 70
+    lower: 20
