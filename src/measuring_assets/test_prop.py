b4c3f7e4414d8c3342a0c86bc6eb91915fd493d6
cwlms1234 <chwilms@web.de>
2024-11-22 22:29:28 +0100

    formatting

--- src/measuring_assets/test_prop.py
+++ src/measuring_assets/test_prop.py
@@ -16,10 +16,14 @@ def fetch_temperature_measuring_test(test_dict: dict, last_value: int = 20) -> i
 
     return random.randrange(rand_min, rand_max, 1)
 
-def produce_test_log(): # python -c 'from src.measuring_assets.test_prop import produce_test_log; produce_test_log()'  # noqa: E501
+
+def produce_test_log():  # python -c 'from src.measuring_assets.test_prop import produce_test_log; produce_test_log()'  # noqa: E501
     timestamp = datetime.now()
     time_cursor = timestamp - timedelta(days=1)
     while time_cursor < timestamp:
         time_cursor += timedelta(seconds=30)
         temp_measure = fetch_temperature_measuring_test({"upper": 70, "lower": 20})
-        write_to_file({"csv_folder_path": "logs/"},(time_cursor, temp_measure))
+        write_to_file(
+            {"execution_specs": {"csv_folder_path": "logs/"}},
+            (time_cursor, temp_measure),
+        )
--- src/measuring_assets/utils/measuring_utils.py
+++ src/measuring_assets/utils/measuring_utils.py
@@ -6,14 +6,16 @@ import pytz
 
 
 def get_timestamp():
-    # Get the current time in the Berlin time zone 
-    berlin_tz = pytz.timezone('Europe/Berlin') 
+    # Get the current time in the Berlin time zone
+    berlin_tz = pytz.timezone("Europe/Berlin")
     return datetime.now(berlin_tz).strftime("%d-%m-%Y %H:%M:%S")
+
 
 def get_date():
     # Get the current year and month
-    berlin_tz = pytz.timezone('Europe/Berlin') 
+    berlin_tz = pytz.timezone("Europe/Berlin")
     return datetime.now(berlin_tz).strftime("%m-%Y")
+
 
 def get_last_n_months_files(n):
     # Get the csv files for the last n months, including the current
@@ -26,6 +28,7 @@ def get_last_n_months_files(n):
         month, year = get_last_month(month, year)
     return last_n_months
 
+
 def get_last_month(month, year):
     if month == 1:
         last_month = 12
@@ -35,19 +38,23 @@ def get_last_month(month, year):
         last_year = year
     return (last_month, last_year)
 
-def write_to_file(config, data):
 
+def write_to_file(config, data):
     # Create specified file path if it doesn't already exist
     os.makedirs(config["execution_specs"]["csv_folder_path"], exist_ok=True)
 
     # Returns the current YYYY-MM, so there will be new log file each month
     date = get_date()
 
-    with open(f"{config["execution_specs"]["csv_folder_path"]}/{date}.csv", "a", newline="") as file:
+    with open(
+        f"{config["execution_specs"]["csv_folder_path"]}/{date}.csv", "a", newline=""
+    ) as file:
         writer = csv.writer(file)
         # Determine whether the file already exists:
         if not file.tell():
-            writer.writerow((config["csv_headers"]["timestamp"], config["csv_headers"]["reading"]))
+            writer.writerow(
+                (config["csv_headers"]["timestamp"], config["csv_headers"]["reading"])
+            )
         else:
             pass
         writer.writerow(data)
