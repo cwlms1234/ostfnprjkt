e4d5614c6b7051af68d3b8cd8a6c10f1a3ac6879
Christoph Wilms <chwilms@web.de>
2025-01-28 14:12:10 +0100

    deleted obsolete files

--- src/measuring_assets/test_prop.py
+++ src/measuring_assets/test_prop.py
@@ -10,16 +10,3 @@ def fetch_temperature_measuring_test(test_dict: dict) -> int:
 
     return random.randrange(min_limit, max_limit, 1)
 
-
-# def produce_test_log( # TODO probably remove
-#     config,
-# ):  # python -c 'from src.measuring_assets.test_prop import produce_test_log; produce_test_log()'  # noqa: E501
-#     timestamp = get_timestamp()
-#     time_cursor = timestamp - timedelta(days=1)
-#     while time_cursor < timestamp:
-#         time_cursor += timedelta(seconds=30)
-#         temp_measure = fetch_temperature_measuring_test({"upper": 70, "lower": 20})
-#         write_to_file(
-#             config,
-#             (time_cursor, temp_measure),
-#         )
