1a1a476d5832907da1df1105120ca6c1bd0ee549
cwlms1234 <chwilms@web.de>
2025-02-15 23:18:08 +0100

    prepared live reading functions in measuring utils

--- src/temperature_control/utils/measurering_utils.py
+++ src/temperature_control/utils/measurering_utils.py
@@ -8,16 +8,25 @@ from temperature_control.utils.test_utils import (
     fetch_temperature_measuring_test,
 )
 
+def get_reading_from_file(filename:str) -> str:
+    with open(filename, "r") as f:
+        return f.read()
+
 
 def get_current_humidity():
+    # reading = get_reading_from_file("sys/bus/iio:device0/in_humidityrelative_input")
+    # return float(reading)/1000
     return fetch_humidity_test()
-    # /1000
 
 
 def get_current_pressure():
+    # reading = get_reading_from_file("/sys/bus/iio/devices/iio:device0/in_pressure_input")
+    # return float(reading)*10
     return fetch_humidity_test()  # *10 hpa
 
 
 def get_current_temperature(config: dict) -> float:
-    # /1000
+    # reading = get_reading_from_file("sys/bus/iio/devices/iio:device0_in_temp_input")
+    # return float(reading)/1000
     return fetch_temperature_measuring_test(config["test_values"])
+
